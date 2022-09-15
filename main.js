ddcd8cb861e8adf76aeaba2ce4880743cc0b942a
Bartek0074 <bartekdo123@gmail.com>
2022-09-15 20:54:57 +0200

    added new button

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
-const userImage = document.getElementById('userImage');
-
-let user;
-let userImageURL;
+const userImage = document.getElementById('user__image');
+const userValue = document.getElementById('user__value');
+const userRefresh = document.querySelector('.user__refresh');
 
 $.ajax({
   url: 'https://randomuser.me/api/',
   dataType: 'json',
   success: function(data) {
     console.log(data);
-    user = data;
-    userImageURL = data.results[0].picture.large;
-    userImage.src = userImageURL;
+    userImage.src = data.results[0].picture.large;
+    userValue.innerText = `${data.results[0].name.first} ${data.results[0].name.last}`
   }
   });
 
+const refresh = () => {
+  $.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function(data) {
+      userImage.src = data.results[0].picture.large;
+      userValue.innerText = `${data.results[0].name.first} ${data.results[0].name.last}`
+    }
+    });
+}
+
+userRefresh.addEventListener('click', refresh);
